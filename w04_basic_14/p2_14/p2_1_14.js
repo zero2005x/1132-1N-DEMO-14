5d429e3484cbf116efeedbc5b117a7e356a34b94
zero2005x <zero2005x@gmail.com>
2025-03-13 20:51:39 +0800

    W04-P4 : Show toSorted data and a stat object to store statistical data

--- w04_basic_14/p2_14/p2_1_14.js
+++ w04_basic_14/p2_14/p2_1_14.js
@@ -1,6 +1,20 @@
 import { sdata, sdata2 } from "./data_14.js";
 
+const toSorted = sdata2.toSorted(function (a, b) {
+  // sorted
+  return b - a;
+});
 console.log("sdata", sdata);
+console.log("sdata2", sdata2);
+console.log("toSorted", toSorted);
+
+let stat = {
+  pass: 0,
+  fail: 0,
+  sum: 0,
+  average: 0,
+  highest: 0,
+};
 
 const result1 = document.querySelector(".result1");
 const result2 = document.querySelector(".result2");
@@ -55,15 +69,32 @@ const getAverage = (s) => {
 
 const outputStat1 = (s) => {
   result1.innerHTML = `<h3 class="my-4">Array Statistics</h3>
-  <p> sdata original: </p>
+  <p> sdata to be sorted: </p>
   <p> ${JSON.stringify(s)}</p>
   <p>Total: ${s.length}</p>
-  <p>Pass: ${computePass(s)}</p>
-  <p>Fail: ${computeFail(s)}</p>
-  <p>Height: ${getHighest(s)}</p>
-  <p>Lowerst: ${getLowest(s)}</p>
-  <p>Average: ${getAverage(s)}</p>
-  <p> sdata length: ${s.length} </p>`;
+  <p>Pass: ${stat.pass}</p>
+  <p>Fail: ${stat.fail}</p>
+  <p>Height: ${stat.highest}</p>
+  <p>Lowerst: ${s[s.length - 1]}</p>
+  <p>Average: ${stat.average.toFixed(1)}</p>
+  <p> sdata length: ${s.length - 1} </p>`;
 };
 
-outputStat1(sdata2);
+const computeStat = (s) => {
+  s.forEach((score) => {
+    stat.sum += score;
+    if (score > stat.highest) {
+      stat.highest = score;
+    } else if (score < stat.lowest) {
+      stat.lowest = score;
+    }
+    if (score >= 60) {
+      stat.pass++;
+    } else {
+      stat.fail++;
+    }
+  });
+  stat.average = stat.sum / s.length;
+};
+computeStat(toSorted);
+outputStat1(toSorted);

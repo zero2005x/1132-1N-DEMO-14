b4228ce16ba38c593b8ae22c5c34fb682551b306
zero2005x <zero2005x@gmail.com>
2025-03-13 20:12:12 +0800

    W04-P3 :Show sdata statistics from js code

--- w04_basic_14/p2_14/data_14.js
+++ w04_basic_14/p2_14/data_14.js
@@ -1,4 +1,5 @@
 export const sdata = [90, 100, 60, 40, 20, 80];
+export const sdata2 = [80, 90, 60, 40, 20, 70, 30, 100, 10, 50];
 
 export const students = [
   {
--- w04_basic_14/p2_14/p2_1_14.js
+++ w04_basic_14/p2_14/p2_1_14.js
@@ -1,21 +1,69 @@
-import { sdata } from "./data_14.js";
+import { sdata, sdata2 } from "./data_14.js";
 
 console.log("sdata", sdata);
 
 const result1 = document.querySelector(".result1");
 const result2 = document.querySelector(".result2");
 
+const computePass = (s) => {
+  let pass = 0;
+  s.forEach((score) => {
+    if (score >= 60) {
+      pass++;
+    }
+  });
+  return pass;
+};
+
+const computeFail = (s) => {
+  let fail = 0;
+  s.forEach((score) => {
+    if (score < 60) {
+      fail++;
+    }
+  });
+  return fail;
+};
+
+const getHighest = (s) => {
+  let highest = s[0];
+  s.forEach((score) => {
+    if (score > highest) {
+      highest = score;
+    }
+  });
+  return highest;
+};
+
+const getLowest = (s) => {
+  let lowest = s[0];
+  s.forEach((score) => {
+    if (score < lowest) {
+      lowest = score;
+    }
+  });
+  return lowest;
+};
+
+const getAverage = (s) => {
+  let total = 0;
+  s.forEach((score) => {
+    total += score;
+  });
+  return total / s.length;
+};
+
 const outputStat1 = (s) => {
   result1.innerHTML = `<h3 class="my-4">Array Statistics</h3>
   <p> sdata original: </p>
   <p> ${JSON.stringify(s)}</p>
-  <p>Total: 55</p>
-  <p>Pass: 4</p>
-  <p>Fail: 2</p>
-  <p>Height: 100</p>
-  <p>Lowerst: 20</p>
-  <p>Average: 65.2</p>
+  <p>Total: ${s.length}</p>
+  <p>Pass: ${computePass(s)}</p>
+  <p>Fail: ${computeFail(s)}</p>
+  <p>Height: ${getHighest(s)}</p>
+  <p>Lowerst: ${getLowest(s)}</p>
+  <p>Average: ${getAverage(s)}</p>
   <p> sdata length: ${s.length} </p>`;
 };
 
-outputStat1(sdata);
+outputStat1(sdata2);

4554eaf4781bce692bf499aeeba9f8315ff74029
zero2005x <zero2005x@gmail.com>
2025-05-22 19:50:26 +0800

    W14-P1: show first welcome page

---
+++ demo/w14_fullstack_14/server_14/server_14.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app_14 = express();
+
+app_14.use(express.static("public"));
+app_14.set("view engine", "ejs");
+
+app_14.use("/product_14/static", (req, res, next) => {
+  console.log("static");
+  res.render("product_14/static", {
+    title: "Get Product - Static",
+    name: "Liang-Ting Lin",
+    id: "913410014",
+  });
+});
+
+app_14.use("/", (req, res, next) => {
+  res.render("index", {
+    title: "Express Server",
+    name: "Liang-Ting Lin",
+    id: "913410014",
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app_14.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
